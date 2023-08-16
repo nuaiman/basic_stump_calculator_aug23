1d1d0c878c8d76f3c108d69c780dec153a24af11
Nuaiman Ashiq <nuaimanashiq.work@gmail.com>
2023-08-16 20:31:57 +0600

    1st commit

---
+++ android/app/src/main/kotlin/com/example/basic_stump_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.basic_stump_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'features/stump_calcualtor/view/stump_size.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    Future.delayed(
+      const Duration(seconds: 5),
+      () {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(
+              builder: (context) => const StumpSize(),
+            ),
+            (route) => false);
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFFa4b0a2),
+              Color(0xFF5a635c),
+            ],
+          ),
+        ),
+        child: Center(
+          child: SvgPicture.asset('assets/images/3DTree.svg'),
+        ),
+      ),
+    );
+  }
+}
